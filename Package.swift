b997dba9f88a6cc997495489e4581e2131801ae0
Medhat Riad <medhat.riad@gmail.com>
2021-01-19 21:09:27 +0100

    Update supported iOS version (SwiftUI from v13)

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "blitzEngine",
     platforms: [
-        .iOS(SupportedPlatform.IOSVersion.v12)
+        .iOS(SupportedPlatform.IOSVersion.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
